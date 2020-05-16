c2062856b67f0c62d4a0d33776356864486b99ed
Jaewook08 <caspi811@gmail.com>
2020-05-16 16:19:11 +0900

    Put header files in Team4Passes.h

--- src/Team4Passes.h
+++ src/Team4Passes.h
@@ -12,7 +12,9 @@
 #include "llvm/Analysis/TargetLibraryInfo.h"
 #include "llvm/IR/DataLayout.h"
 #include "llvm/Pass.h"
-#include "llvm/PassAnalysisSupport.h"
+#include "llvm/Analysis/ValueTracking.h"
+#include "llvm/IR/Constants.h"
+#include <algorithm>
 
 using namespace std;
 using namespace llvm;
@@ -21,7 +23,7 @@ using namespace llvm::PatternMatch;
 class Malloc2AllocPass : public llvm::PassInfoMixin<Malloc2AllocPass> {
 public:
     void findPossibleMallocs(Function &F, vector<Instruction*> &PossibleMallocs);
-    void findReplaceableMallocs(Function &F, vector<Instruction*> &PossibleMallocs, vector<Instruction*> &ReplaceableMallocs, vector<Instruction*> &RemovableFrees);
+    void findReplaceableMallocs(Function &F, FunctionAnalysisManager &FAM, vector<Instruction*> &PossibleMallocs, vector<Instruction*> &ReplaceableMallocs, vector<Instruction*> &RemovableFrees);
     void replaceMallocwithAlloc(Function &F, FunctionAnalysisManager &FAM, vector<Instruction*> &ReplaceableMallocs, vector<Instruction*> &RemovableFrees);
     PreservedAnalyses run(Function &F, FunctionAnalysisManager &FAM);
 };
