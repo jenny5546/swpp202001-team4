44606f2f9a06db4e71b057c866fa34e872199060
Jaeeun Lee <jenny5546@naver.com>
2020-05-31 12:02:10 +0900

    rmq all reduced, bitcount 1,3 bubblesort,prime reduced

--- src/FunctionInlinePass.cpp
+++ src/FunctionInlinePass.cpp
@@ -8,11 +8,23 @@
 
 ********************************************/ 
 
-unsigned FunctionInlinePass::instsInFunc(const Function &F) {
+unsigned FunctionInlinePass::countInsts(const Function &F) {
     unsigned count=0;
     for (auto &BB : F) {
         for (auto &I : BB){
             count++;
+        }
+    }
+    return count;
+}
+
+unsigned FunctionInlinePass::countRegs(const Function &F) {
+    unsigned count=0;
+    for (auto &BB : F) {
+        for (auto &I : BB){
+            if (I.hasName() || !I.hasName() && !I.getType()->isVoidTy()) { 
+                count++; 
+            }
         }
     }
     return count;
@@ -39,7 +51,7 @@ PreservedAnalyses FunctionInlinePass::run(Module &M, ModuleAnalysisManager &MAM)
                 if (auto *CI = dyn_cast<CallInst>(&I)){
                     Function *calledfunc = CI->getCalledFunction();
                     // if (instsInFunc(*calledfunc)<15)
-                    if (instsInFunc(*calledfunc)<20 && instsInFunc(F)<20)
+                    if (countRegs(*calledfunc)<15 && countRegs(F)<15)
                         CalledFunctions.push_back(calledfunc);
                 }
             }
@@ -71,6 +83,22 @@ PreservedAnalyses FunctionInlinePass::run(Module &M, ModuleAnalysisManager &MAM)
         F->removeDeadConstantUsers();
         return !F->isDefTriviallyDead();
     });
+
+    // Delete the non-comdat ones from the module and also from our vector.
+    auto NonComdatBegin = partition(
+        InlinedFunctions, [&](Function *F) { return F->hasComdat(); });
+    for (Function *F : make_range(NonComdatBegin, InlinedFunctions.end()))
+        M.getFunctionList().erase(F);
+    InlinedFunctions.erase(NonComdatBegin, InlinedFunctions.end());
+
+    if (!InlinedFunctions.empty()) {
+        // Now we just have the comdat functions. Filter out the ones whose comdats
+        // are not actually dead.
+        filterDeadComdatFunctions(M, InlinedFunctions);
+        // The remaining functions are actually dead.
+        for (Function *F : InlinedFunctions)
+        M.getFunctionList().erase(F);
+    }
 
     return Inlined ? PreservedAnalyses::none() : PreservedAnalyses::all();
 }
